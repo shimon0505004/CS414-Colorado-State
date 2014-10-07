596ea7528af81f87fc97c38752d9719521017fc2
Nathan Lighthart <nlighthart@gmail.com>
2014-10-07 12:05:52 -0600

    Added Skeleton maven project. I have tested it in netbeans and eclipse. Added to the .gitignore file the eclipse and netbeans project files.

---
+++ PizzaOrderingSystem/src/main/java/cs414/pos/Main.java
@@ -0,0 +1,14 @@
+package cs414.pos;
+
+/**
+ *
+ * @author Nathan Lighthart
+ */
+public class Main {
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
