8020fac7d4e242c0aa5b63a44b3caed922de6d49
tcqiuyu <tcqiuyu2@gmail.com>
2014-10-22 03:53:31 -0600

    ItemTest

---
+++ PizzaOrderingSystem/src/test/java/cs414/pos/ItemTest.java
@@ -0,0 +1,79 @@
+package cs414.pos;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ItemTest {
+
+	String itemName1, itemName2;
+	double itemPrice1, itemPrice2;
+	String itemDesc1, itemDesc2;
+	Item item1, item2;
+
+	public void setUp() {
+		itemName1 = "N1";
+		itemPrice1 = "100";
+		itemDesc1 = "Cheese Pizza";
+		item1 = new Item(itemName1, itemPrice1, itemDesc1);
+	}
+
+	public void testGetName() {
+		assertEquals(itemName1, item1)
+	}
+
+	public void testSetName() {
+
+	}
+
+	public void testSetNullName() {
+
+	}
+
+	public void testGetItemDesc() {
+
+	}
+
+	public void testGetNullItemDesc() {
+
+	}
+
+	public void testSetItemDesc() {
+
+	}
+
+	public void testSetNullItemDesc() {
+
+	}
+
+	public void testGetBasePrice() {
+
+	}
+
+	public void testSetBasePrice(){
+		
+	}
+	
+	public void testGetDiscoutPrice() {
+
+	}
+
+	public void testIsSpecial() {
+
+	}
+
+	public void testSetSpecial() {
+
+	}
+
+	public void testSetNegSpecial() {
+
+	}
+
+	public void testRemoveSpecial() {
+
+	}
+
+}
