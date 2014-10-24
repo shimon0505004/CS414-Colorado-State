bbc1b9ffb23eea4a62f2f3dde228bae93603d375
tcqiuyu <tcqiuyu2@gmail.com>
2014-10-24 09:07:57 -0600

    StoreTest

--- PizzaOrderingSystem/src/test/java/cs414/pos/StoreTest.java
+++ PizzaOrderingSystem/src/test/java/cs414/pos/StoreTest.java
@@ -23,6 +23,9 @@ public class StoreTest {
 
 	String menuName1, menuName2, menuDesc1, menuDesc2;
 	Item test_item1, test_item2;
+	
+	Kiosk kiosk1, kiosk2;
+	
 	@Before
 	public void setUp() throws Exception {
 		testStore1 = new Store();
@@ -53,8 +56,11 @@ public class StoreTest {
 		menuName2 = "Menu2";
 		menuDesc1 = "MenuDesc1";
 		menuDesc2 = "MenuDesc2";
-		Item test_item1 = new Item("item1", 10, "Test Item 1");
-		Item test_item2 = new Item("item2", 5, "Test Item 2");
+		test_item1 = new Item("item1", 10, "Test Item 1");
+		test_item2 = new Item("item2", 5, "Test Item 2");
+	
+		kiosk1 = new Kiosk(1, testStore3);
+		kiosk2 = new Kiosk(2, testStore3);
 	}
 
 	@After
@@ -239,7 +245,7 @@ public class StoreTest {
 		String testLastName1 = "Shimon";
 		String phoneNumber = "888-888-8888";
 		assertEquals(0, testStore4.getMembers().size());
-		Customer newCustomer = testStore4.addNewMember(testFirstName1, testFirstName1, phoneNumber);
+		Customer newCustomer = testStore4.addNewMember(testFirstName1, testLastName1, phoneNumber);
 		assertEquals(1, testStore4.getMembers().size());
 			
 		
@@ -250,7 +256,7 @@ public class StoreTest {
 		String testFirstName1 = "Shimon";
 		String testLastName1 = "Shimon";
 		String phoneNumber = "888-888-8888";
-		Customer newCustomer = testStore4.addNewMember(testFirstName1, testFirstName1, phoneNumber);
+		Customer newCustomer = testStore4.addNewMember(testFirstName1, testLastName1, phoneNumber);
 		assertEquals(newCustomer,testStore4.getMember(newCustomer.getMemberShipNumber()));
 	}
 
@@ -348,18 +354,55 @@ public class StoreTest {
 
 	@Test
 	public void testAddMenuItem() {
-		fail("Not yet implemented");
+		Employee test_Employee1 = testStore3.addEmployee(testName1, testLoginID1, testPassWord1, Privilege.Cashier);	
+		Employee test_Employee2 = testStore3.addEmployee(testName2, testLoginID2, testPassWord2,Privilege.Chef);	
+		Employee test_Employee3 = testStore3.addEmployee(testName3, testLoginID3, testPassWord3,Privilege.Manager);
+		Menu m1 = testStore3.defineMenu(test_Employee3, menuName1, menuDesc1);
+		assertEquals(0, testStore3.getSetOfMenus().size());
+		testStore3.addMenuItem(test_Employee3, m1, "item1", 10, "test item 1");
+		assertEquals(1, testStore3.getSetOfMenus().size());
+		testStore3.addMenuItem(test_Employee3, m1, "item2", 8, "test item 2");
+		assertEquals(2, testStore3.getSetOfMenus().size());
+		testStore3.addMenuItem(test_Employee2, m1, "item3", 15, "test item 3");
+		assertEquals(2, testStore3.getSetOfMenus().size());
+		testStore3.addMenuItem(test_Employee1, m1, "item4", 19, "test item 4");
+		assertEquals(2, testStore3.getSetOfMenus().size());
 	}
 
 	@Test
 	public void testGetSetOfKiosk() {
-		fail("Not yet implemented");
+		assertEquals(0, testStore4.getSetOfKiosk().size());
+		assertEquals(2, testStore3.getSetOfKiosk());
 	}
 
 	@Test
 	public void testSetSetOfKiosk() {
-		fail("Not yet implemented");
+		Kiosk tempKiosk1 = new Kiosk(3);
+		Kiosk tempKiosk2 = new Kiosk(4);
+		Set<Kiosk> kioskSet = new HashSet<Kiosk>();
+		kioskSet.add(tempKiosk1);
+		kioskSet.add(tempKiosk2);
+		assertEquals(0, testStore4.getSetOfKiosk());
+		testStore4.setSetOfKiosk(kioskSet);
+		assertEquals(2, testStore4.getSetOfKiosk());
+		assertTrue(testStore4.getSetOfKiosk().contains(tempKiosk1));
+		assertTrue(testStore4.getSetOfKiosk().contains(tempKiosk2));
 	}
+	
+	@Test
+	public void testAddKiosk(){
+		Employee test_Employee1 = testStore3.addEmployee(testName1, testLoginID1, testPassWord1, Privilege.Cashier);	
+		Employee test_Employee2 = testStore3.addEmployee(testName2, testLoginID2, testPassWord2,Privilege.Chef);	
+		Employee test_Employee3 = testStore3.addEmployee(testName3, testLoginID3, testPassWord3,Privilege.Manager);
+		assertEquals(2, testStore3.getSetOfKiosk().size());
+		testStore3.addKiosk(test_Employee1, 3);
+		assertEquals(2, testStore4.getSetOfKiosk().size());
+		testStore3.addKiosk(test_Employee2, 4);
+		assertEquals(2, testStore4.getSetOfKiosk().size());
+		testStore3.addKiosk(test_Employee3, 5);
+		assertEquals(3, testStore4.getSetOfKiosk().size());
+	}
+	
 
 	@Test
 	public void testGetSetOfRegister() {
@@ -371,6 +414,12 @@ public class StoreTest {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void testAddRegister(){
+		
+	}
+	
+	
 	@Test
 	public void testGetSetOfPlacedOrder() {
 		fail("Not yet implemented");
