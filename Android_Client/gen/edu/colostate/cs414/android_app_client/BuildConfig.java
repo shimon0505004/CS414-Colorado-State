f49ac29059d6d10a627c301f66bf9ea832f380c6
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2014-11-11 13:46:13 -0700

    Created an Eclipes based android application
    In the main program directory, an application is created in android.

---
+++ Android_Client/gen/edu/colostate/cs414/android_app_client/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.colostate.cs414.android_app_client;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
